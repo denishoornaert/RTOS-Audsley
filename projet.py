c9dbc66d0c4b81450d0b49aaa771573dd31e4bb1
Denis <dhoornae@ulb.ac.be>
2017-12-01 19:03:27 +0100

    Update so that it benefits from the new classes

--- projet.py
+++ projet.py
@@ -2,23 +2,16 @@
 # -*- coding:Utf8 -*-
 
 import sys
-
-class Task:
-
-    def __init__(self, offset, period, deadline, wcet):
-        self._offset = offset
-        self._period = period
-        self._deadline = deadline
-        self._wcet = wcet
-
-    def __str__(self):
-        return "Offset: " + str(self._offset) + " - Period: " + str(self._period) + \
-                " - Deadline: " + str(self._deadline) + " - WCET: " + str(self._wcet)
+from Configuration import *
+from FileReader import *
 
 
 class Main:
 
     def __init__(self, argv):
+
+        # Set debug mode
+        debug = False;
 
         # Remove program name
         if(len(argv) > 2):
@@ -26,8 +19,10 @@ class Main:
         else:
             raise AttributeError("Missing argument")
 
+        config = FileReader.produceConfig(argv[1]);
+
         if(argv[0] == "interval"):
-            pass
+            print(config.feasibilityInterval());
         elif(argv[0] == "sim"):
             pass
         elif(argv[0] == "audsley"):
@@ -38,36 +33,9 @@ class Main:
             raise AttributeError("Unknow argument: " + str(argv[0]))
 
         # ONLY FOR TEST [DEBUG]
-        taskList = self.readTaskFile(argv[1])
-        for task in taskList:
-            print(task)
-
-        print(argv)
-    
-
-    def readTaskFile(self, fileName):
-        """
-            Read the Task File and create a python list of Task
-
-            :param fileName name of the file
-            :return list of Task
-        """
-        listOfTask = []
-
-        with open(fileName, "r") as f:
-            lineIndex = 0
-            for line in f:
-                strTask = line.strip()
-                splitTask = strTask.split()
-                if(len(splitTask) != 4):
-                    print("Error in line " + str(lineIndex))
-                else:
-                    listOfTask.append(Task(splitTask[0], splitTask[1], splitTask[2], splitTask[3]))
-
-                lineIndex += 1
-
-        f.close()
-        return listOfTask
+        if(debug):
+            print(config);
+            print(argv)
 
 
 
