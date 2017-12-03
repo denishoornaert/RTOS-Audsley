c1108a7b2983251b659500e10e04b7b33679238a
Denis <dhoornae@ulb.ac.be>
2017-12-03 11:54:37 +0100

    now uses matplotlib (more convinient)

--- OutputFactory.py
+++ OutputFactory.py
@@ -1,14 +1,13 @@
 import numpy
-import toyplot
-import toyplot.png
+import matplotlib.pylab as plt
 
 class OutputFactory ():
 
     """docstring for OutputFactory."""
 
     @staticmethod
-    def toFile(canvas, filePath):
-        toyplot.png.render(canvas, filePath)
+    def toFile(plt, filePath):
+        plt.savefig(filePath, bbox_inches='tight');
 
     @staticmethod
     def toMatrix(simulator):
@@ -18,21 +17,18 @@ class OutputFactory ():
             if(elem != None):
                 res[elem.priority][i] = elem.priority+1;
         res.reverse();
-        return res;
+        return numpy.matrix(res);
 
     @staticmethod
-    def setColors(matrix):
-        canvas, mark = toyplot.matrix(matrix, label="Scheduling", tlabel="Time slots",  llabel="Tasks");
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if(matrix[i][j]):
-                    mark.body.cell[i, j].style = {"fill":"black"};
-                else:
-                    mark.body.cell[i, j].style = {"fill":"white"};
-        return canvas, mark;
+    def generateFigure(matrix):
+        fig = plt.figure();
+        ax = fig.add_subplot(1,1,1);
+        ax = fig.add_subplot(1,1,1);
+        plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.get_cmap('hot_r'));
+        return plt;
 
     @staticmethod
     def produce(simulator, filePath):
         matrix = OutputFactory.toMatrix(simulator);
-        canvas, mark = OutputFactory.setColors(matrix);
-        OutputFactory.toFile(canvas, filePath);
+        fig = OutputFactory.generateFigure(matrix);
+        OutputFactory.toFile(fig, filePath);
