dc27f8058b2b2b2d837c02017863a1314700157b
Denis <dhoornae@ulb.ac.be>
2017-12-03 18:23:35 +0100

    Add a method to get the utilisation of the task

--- Task.py
+++ Task.py
@@ -15,3 +15,6 @@ class Task ():
     def __str__(self):
         t = "Offset : {0}\tPeriod : {1}\tDeadline : {2}\tWCET : {3}\tPriority : {4}\n";
         return t.format(self.offset, self.period, self.deadline, self.wcet, self.priority);
+
+    def utilisation(self):
+        return self.wcet/self.period;
